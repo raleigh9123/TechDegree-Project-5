ae8fac39c8456fe23fb3968c5df0bd9d1cd06b52
Joseph Butterfield <raleigh9123@gmail.com>
2020-01-24 21:13:33 -0800

    Generate HTML Content
    Generate card and modal containers from random user generator api json data

--- scripts.js
+++ scripts.js
@@ -52,3 +52,71 @@ fetch(`${randomUserURL}?results=${numUsers}`)
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
+const gallery = document.querySelector('.gallery');
+
+const generateGallery = users => {
+    users.results.map(person => {
+        const cardHTML = `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="${person.picture.large}" alt="${person.name.first} ${person.name.last}">
+            </div>
+            <div class="card-info-container">
+                <h3 id="${person.name.first}-${person.name.last}" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="card-text">${person.email}</p>
+                <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+            </div>
+        </div>
+        `;
+
+        gallery.innerHTML += cardHTML;
+    });
+};
+
+const generateModals = users => {
+    let modalContainer = document.createElement('div');
+    modalContainer.className = 'modal-container';
+    // modalContainer.style = 'display:none;';
+
+    users.results.map(person => {
+        console.log(person);
+
+        const date = person.dob.date.toString();
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+        const monthDigit = parseInt(date.slice(5, 7));
+        const month = months[monthDigit - 1];
+        const day = date.slice(8, 10);
+        const year = date.slice(0, 4);
+
+        modalContainer.innerHTML += `
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${person.picture.large}" alt="${person.name.first} ${person.name.last}">
+                        <h3 id="${person.name.first}-${person.name.last}" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                        <p class="modal-text">${person.email}</p>
+                        <p class="modal-text cap">${person.location.city}</p>
+                        <hr>
+                        <p class="modal-text">${person.phone}</p>
+                        <p class="modal-text">${person.location.street.number} ${person.location.street.name}<br>${person.location.city}, ${person.location.state} ${person.location.postcode}</p>
+                        <p class="modal-text">Birthday: ${month} ${day}, ${year}</p>
+                    </div>
+                </div>
+            `;
+        gallery.parentNode.insertBefore(modalContainer, gallery.nextElementSibling);
+    });
+};
